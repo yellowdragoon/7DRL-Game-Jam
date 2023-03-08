85ef7772ca1c2f3b7eef8caf4190062147e22fbc
Henry Li <lih8@tcd.ie>
2023-03-08 00:22:53 +0000

    Generated map in world

--- 7DRL Jam Game/Assets/Scripts/TilemapGen.cs
+++ 7DRL Jam Game/Assets/Scripts/TilemapGen.cs
@@ -6,32 +6,35 @@ using Random = UnityEngine.Random;
 
 public class TilemapGen : MonoBehaviour
 {
-    [SerializeField] private Tilemap map;
+    [SerializeField] private Tilemap tileMap;
     [SerializeField] private TileBase floorTile;
     [SerializeField] private TileBase wallTile;
 
-
-
-
     // Start is called before the first frame update
     void Start()
-    {
-
-        //Tilemap tm = GetComponentInChildren<Tilemap>();
-        //tm.SetTile(new Vector3Int(0, 0, 0), (Tile)Resources.Load("Sprites/image_17", typeof(Tile)));
-        //tm.SetTile(new Vector3Int(0, 1, 0), (Tile)Resources.Load("image_43", typeof(Tile)));
-        //tm.RefreshAllTiles();
+    { 
 
         var map = CellularAutomata.Generate();
         Debug.Log("Map generated");
         CellularAutomata.PrintMap(map);
         Debug.Log(Cell.printChars[map[0, 0]]);
+
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                if(map[i, j] == Cell.Type.Floor)
+                { 
+                    tileMap.SetTile(new Vector3Int(i, j, 0), floorTile);
+                }
+                else
+                {
+                    tileMap.SetTile(new Vector3Int(i, j, 0), wallTile);
+                }
+                
+            }
+        }
     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
